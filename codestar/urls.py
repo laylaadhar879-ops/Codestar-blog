03e57c5f86f4a6fe7e7f8c97cc1ffdffd49f6c53
laylaadhar879-ops <laylaadhar879@gmail.com>
2026-02-16 15:24:29 +0000

    edit to my_blog

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,5 +20,8 @@ from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog_views.my_blog, name="blog"), # the app urls are loaded as the main urls
+    path('', blog_views.blog, name="home"),  # root URL
+    path('blog/', blog_views.blog, name="blog"),  # blog page
 ]
+
+
