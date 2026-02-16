f643a2d85cbca0a0f97b318b2f1e264e162ef5d0
laylaadhar879-ops <laylaadhar879@gmail.com>
2026-02-16 13:03:49 +0000

    Add home view and fix URLs

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
+def blog(request):
     return HttpResponse("Welcome to Codestar Blog!")
