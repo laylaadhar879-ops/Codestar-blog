9c2bd506a37e0f446ca798d6a86faf8c84e5287b
laylaadhar879-ops <laylaadhar879@gmail.com>
2026-02-16 12:09:03 +0000

    Make blog homepage

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,11 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "=N*l6qV$EvqxUyBOuSt&&QYrWe#vlvm2@ZTM(*fNUYPvAzVHj*")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',
-                 '127.0.0.1',]
-
+ALLOWED_HOSTS = ['codestar-blogg.herokuapp.com'] 
 
 # Application definition
 
@@ -47,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
