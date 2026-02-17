5cb63b14bcae1f02d6d45123bc1922d47fa018d3
laylaadhar879-ops <laylaadhar879@gmail.com>
2026-02-17 15:20:06 +0000

    added comment CRUD

--- about/models.py
+++ about/models.py
@@ -8,3 +8,17 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
+
--- codestar/settings.py
+++ codestar/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+
 if os.path.isfile('env.py'):
-    import env
+    import env  # noqa: F401
+    env  # noqa: F401
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "=N*l6qV$EvqxUyBOuSt&&QYrWe#vlvm2@ZTM(*fNUYPvAzVHj*")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "=N*l6qV$EvqxUyBOuSt&&QYrWe#vlvm2@ZTM(*fNUYPvAzVHj*",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -50,7 +55,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'blog', 
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'blog',
     'django_summernote',
     'about',
 ]
@@ -58,6 +65,9 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -93,7 +103,8 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.2/ref/settings/
+# databases
 
 # DATABASES = {
 #     'default': {
@@ -103,7 +114,14 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "postgresql://neondb_owner:npg_AHCbuM0Dth8k@ep-dawn-bonus-agc3gcsw.c-2.eu-central-1.aws.neon.tech/rise_quack_gown_751974"))
+    'default': dj_database_url.parse(
+        os.environ.get(
+            "DATABASE_URL",
+            "postgresql://neondb_owner:npg_AHCbuM0Dth8k@"
+            "ep-dawn-bonus-agc3gcsw.c-2.eu-central-1.aws.neon.tech/"
+            "rise_quack_gown_751974",
+        )
+    )
 }
 
 CSRF_TRUSTED_ORIGINS = [
@@ -112,20 +130,33 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/4.2/ref/settings/
+# auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
 
@@ -157,3 +188,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static'), ]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
